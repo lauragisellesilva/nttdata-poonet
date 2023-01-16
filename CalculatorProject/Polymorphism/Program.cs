319bb1f428ced9f4e100aeba1340e5355dbe651a
lauragisellesilva <lausilva@emeal.nttdata.com>
2023-01-16 09:31:32 +0100

    Added polymorphism exercise, boxing and unboxing

---
+++ CalculatorProject/Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace CalculatorProject.Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            Student student = new Student();
+            student.Name= "Pepe";
+
+            //Boxing And Autoboxing
+            array.Add(student);
+
+            Console.WriteLine(array);
+
+            Console.WriteLine(((Student)array[0]).Name);
+
+            //array[0].Name error
+            Object obj = new Student();
+            ((Student)obj).Name = "Alberto"; //unboxing
+            Console.WriteLine(((Student)obj).Name);
+        }
+    }
+}
